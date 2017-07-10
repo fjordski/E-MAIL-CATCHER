7ce0b941d87e4377f3f03d4217c5e3ae7e74d4cf
fjordski <fordheacock@gmail.com>
2017-07-10 13:25:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ $(document).ready(function () {
 
 	// process the form
 	$('form').submit(function (event) {
-
-		// Stop form submitting
 		event.preventDefault();
-
-		// Get form tag
 		var form = event.target;
-
-		// Get email address
 		var email = form.elements.email.value;
-
-		// Email valid if has length and has @ in it
 		var valid = email.length > 0 && email.indexOf('@') > 0;
-
-		// If not valid then show errors
+		
 		if (!valid) {
 			$('.input-error-helper').show();
 			$('.input-email').addClass("input-error");
